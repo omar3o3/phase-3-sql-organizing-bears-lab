3b583f23f42e671fec7c46ffe9c5f86bef9dc29e
omar3o3 <otantawy53@gmail.com>
2022-06-13 12:54:19 -0400

    commit tree 6b79119b7815551f6beaabdacb56af20cc7d4b23

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,9 +1,14 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT bears.name, bears.age FROM bears WHERE sex='F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  <<-SQL
+    SELECT
+      bears.name
+    FROM bears
+    ORDER BY bears.name ASC;
+  SQL
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
